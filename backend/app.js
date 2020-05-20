73aa2adc8003e8a7c7588025b3e160a715ddd10e
Christophe Moreira <christophe.moreira@outlook.com>
2020-05-20 19:00:47 +0100

    added mongodbatlas

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,39 @@
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const { ApolloServer, gql } = require('apollo-server-express');
-const express = require('express')
-const cors = require('cors')
+const { ApolloServer, gql } = require("apollo-server-express");
+const express = require("express");
+const cors = require("cors");
 
 const connectDb = require("./db");
-const {typeDefs} = require('./graphql/typeDefs')
-const resolvers = require('./graphql/resolvers')
-const getUserId = require('./utils/getUserId')
+const { typeDefs } = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const getUserId = require("./utils/getUserId");
 const UserModel = require("./models/UserModel");
 const PlaceModel = require("./models/PlaceModel");
 
-const app = express()
+const app = express();
 
 const corsOpt = {
-  origin: '*',
-  credentials:true
-}
+  origin: "*",
+  credentials: true,
+};
 
-app.use(cors())
+app.use(cors());
 
-
-
-const context = ({req, res}) => {
-  
-  return {user: getUserId(req), UserModel, PlaceModel};
-}
+const context = ({ req, res }) => {
+  return { user: getUserId(req), UserModel, PlaceModel };
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context
+  context,
 });
 
 server.applyMiddleware({ app });
 
-// connectDb();
+connectDb();
 
 app.listen(PORT, () =>
   console.log(`🚀 Server ready at http://localhost:${PORT}`)
-)
+);
